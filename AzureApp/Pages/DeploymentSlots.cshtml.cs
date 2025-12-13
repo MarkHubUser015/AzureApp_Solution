693fae1d811ab701725625bfd688e7ba6c8487e6
Mark <mmcleodISphereNet@gmail.com>
2025-12-12 19:34:12 -0500

    Add project files.

---
+++ AzureApp/Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzureApp.Pages
+{
+    public class DeploymentSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
